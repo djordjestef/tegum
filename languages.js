3bd6dc296297736a2648a7dea4e1e12bdffade0c
djordje <djordje.stefanovic89@gmail.com>
2023-11-01 15:38:37 +0100

    nav menu

--- languages.js
+++ languages.js
@@ -1,67 +1,44 @@
 const jsonEN = {
-    storage_sub: ' We offer what everyone seems to be missing — space.',
-  
-  };
-  
-  const jsonSR = {
-    storage_sub: 'Mi imamo ono što svima nedostaje - prostor.',
-   
-  };
-  
-  $('.en').click(function () {
+  storage_sub: ' We offer what everyone seems to be missing — space.',
+};
+
+const jsonSR = {
+  storage_sub: 'Mi imamo ono što svima nedostaje - prostor.',
+};
+
+$('.en').click(function () {
+  console.log('clicked EN');
+  localStorage.setItem('lang', 'en');
+  location.reload();
+});
+
+$('.sr').click(function () {
+  console.log('clicked SR');
+  localStorage.setItem('lang', 'sr');
+  location.reload();
+});
+
+$(document).ready(function () {
+  const isMobileTablet = window.innerWidth < 992;
+
+  if (localStorage.getItem('lang') === null) {
     localStorage.setItem('lang', 'en');
+
+    $('.sr').addClass('active');
     location.reload();
-  });
-  
-  $('.sr').click(function () {
-    localStorage.setItem('lang', 'sr');
-    location.reload();
-  });
-  
-  $(document).ready(function () {
-    const isMobileTablet = window.innerWidth < 992;
-  
-    if (localStorage.getItem('lang') === null) {
-      localStorage.setItem('lang', 'en');
-      if (isMobileTablet) {
-        $('.get_space_en').removeClass('active');
-        $('.get_space_mobile_en').addClass('active');
-      } else {
-        $('.get_space_en').addClass('active');
-        $('.get_space_mobile_en').removeClass('active');
+  } else {
+    if (localStorage.getItem('lang') === 'sr') {
+      $('.en').addClass('active');
+
+      for (let key in jsonSR) {
+        document.querySelector('.' + key).textContent = jsonSR[key];
       }
-  
-      $('.sr').addClass('active');
-      location.reload();
     } else {
-      if (localStorage.getItem('lang') === 'sr') {
-        $('.en').addClass('active');
-        if (isMobileTablet) {
-          $('.get_space_mobile_sr').addClass('active');
-          $('.get_space_sr').removeClass('active');
-        } else {
-          $('.get_space_sr').addClass('active');
-          $('.get_space_mobile_sr').removeClass('active');
-        }
-        for (let key in jsonSR) {
-          document.querySelector('.' + key).textContent = jsonSR[key];
-          $('.form_btn_lang').attr('value', 'Pošalji');
-        }
-      } else {
-        $('.sr').addClass('active');
-        if (isMobileTablet) {
-          $('.get_space_mobile_en').addClass('active');
-          $('.get_space_en').removeClass('active');
-        } else {
-          $('.get_space_en').addClass('active');
-          $('.get_space_mobile_en').removeClass('active');
-        }
-  
-        for (let key in jsonEN) {
-          document.querySelector('.' + key).textContent = jsonEN[key];
-          $('.form_btn_lang').attr('value', 'Send');
-        }
+      $('.sr').addClass('active');
+
+      for (let key in jsonEN) {
+        document.querySelector('.' + key).textContent = jsonEN[key];
       }
     }
-  });
-  
+  }
+});
---
+++ main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+  //accordion
+  gsap.registerPlugin(ScrollTrigger);
+
+  const accordion = gsap.timeline({
+    defaults: {
+      ease: 'none',
+      duration: 1,
+    },
+    scrollTrigger: {
+      trigger: '.acc_container',
+      start: 'top top',
+      end: '+=' + (window.innerHeight * 3 - 900),
+      scrub: true,
+      pin: '.acc_container',
+    },
+  });
+
+  accordion.to('#pin1', { height: 100 }, 0);
+  accordion.to('#pin2', { height: window.innerHeight - 200 }, 0);
+  accordion.to('#pin2', { height: 100 }, 1);
+  accordion.to('#pin3', { height: window.innerHeight - 200 }, 1);
+  accordion.to('#pin3', { height: 100 }, 2);
+
+  gsap.set('.spacer.bot', { marginTop: '-=' + (window.innerHeight - 300) });
+
+  //end of accordion
+});
+
+//navbar
+$(function () {
+  //   $('li.dropdown > a').on('click', function (event) {
+  $(document).on('click', 'li.dropdown > a', function (event) {
+    console.log('clicked');
+    // Toggle the dropdown without preventing default
+    $(this).parent().find('ul').first().toggle(300);
+    $(this).parent('li').addClass('opened');
+    $(this).parent().siblings().find('ul').hide(200);
+    $(this).parent().siblings('li').removeClass('opened');
+    $(this).siblings().find('li').removeClass('opened');
+
+    if ($(this).parent('li').hasClass('opened')) {
+      $(this)
+        .find('img')
+        .css({ transform: 'rotate(' + 180 + 'deg)' });
+      $(this)
+        .parent()
+        .siblings()
+        .find('img')
+        .css({ transform: 'rotate(' + 0 + 'deg)' });
+    } else {
+      $(this)
+        .find('img')
+        .css({ transform: 'rotate(' + 0 + 'deg)' });
+    }
+    event.preventDefault();
+  });
+  var pageScroll = function (e) {
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.main-navigation').outerHeight();
+
+    $(window).scroll(function (e) {
+      didScroll = true;
+    });
+
+    setInterval(function () {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    });
+
+    function hasScrolled() {
+      var st = $(this).scrollTop();
+      var navbarHeight = $('.main-navigation').outerHeight();
+
+      if (Math.abs(lastScrollTop - st) <= delta) return;
+
+      if (st > lastScrollTop && st > navbarHeight) {
+        $('.main-navigation').removeClass('nav-down').addClass('nav-up');
+      } else {
+        if (st + $(window).height() < $(document).height()) {
+          $('.main-navigation').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+
+      lastScrollTop = st;
+    }
+  };
+
+  pageScroll();
+});
