521931240683b348685e2e7e4cbb80b6d2a3bb01
djordje <djordje.stefanovic89@gmail.com>
2023-11-13 21:47:36 +0100

    horizontal scroll

--- main.js
+++ main.js
@@ -170,4 +170,29 @@ $(document).ready(function () {
       $('.background_helper').css('background-color', '#fff').fadeOut(100);
     },
   );
+
+
+  // Adding scroll event listener
+document.addEventListener('scroll', horizontalScroll);
+
+//Selecting Elements
+let sticky = document.querySelector('.sticky');
+let stickyParent = document.querySelector('.sticky-parent');
+
+let scrollWidth = sticky.scrollWidth;
+let verticalScrollHeight = stickyParent.getBoundingClientRect().height-sticky.getBoundingClientRect().height;
+
+//Scroll function 
+function horizontalScroll(){
+
+    //Checking whether the sticky element has entered into view or not
+    let stickyPosition = sticky.getBoundingClientRect().top;
+    if(stickyPosition > 1){
+        return;
+    }else{
+        let scrolled = stickyParent.getBoundingClientRect().top; //how much is scrolled?
+        sticky.scrollLeft =(scrollWidth/verticalScrollHeight)*(-scrolled)*0.85;
+    
+    }
+}
 });
