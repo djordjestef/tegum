6896fa28e2ae7b075214d6932b52caf1b19a8409
djordje <djordje.stefanovic89@gmail.com>
2023-12-13 10:02:10 +0100

    testimonials language

--- languages.js
+++ languages.js
@@ -49,6 +49,9 @@ const jsonEN = {
   about_us_description_2: `Our strategic support empowers your operational efficiency, stay agile and make well-informed decisions. We're your go-to source for optimizing procurement, quality control, and supply chain management.`,
   about_us_title_3: 'Growth and Prosperity',
   about_us_description_3: `With a dedication to compliance, reliable management consulting and insightful financial guidance, we're here to drive your business's growth and profitability for a future of sustainable success.`,
+  testimonials_holder_text_description:
+    '"When you choose us, you choose partner with a proven track record of success in the industrial sector, dedicated to your growth and profitability."',
+    testimonials_holder_text_name:'- Name Surname, Chief Executive Officer at Tegum',
   what_we_do_title_1: '01 - Project Management',
   what_we_do_title_2: '02 - Consulting Services',
   what_we_do_title_3: '03 - Custom Manufacturing',
@@ -467,6 +470,9 @@ const jsonSR = {
   about_us_title_3: 'Rast i prosperitet',
   about_us_description_3:
     'Sa posvećenošću usklađenosti, pouzdanim savetovanjem o menadžmentu i pronicljivim finansijskim uputstvima, mi smo tu da pokrenemo rast i profitabilnost vašeg poslovanja za budućnost održivog uspeha.',
+  testimonials_holder_text_description:
+    '"Kada odaberete nas, birate partnera sa dokazanim uspehom u industrijskom sektoru, posvećen vašem rastu i profitabilnosti."',
+    testimonials_holder_text_name:'- Generalni izvršni direktor Teguma',
   what_we_do_title_1: '01 - Upravljanje projektima',
   what_we_do_title_2: '02 - Konsultantske usluge',
   what_we_do_title_3: '03 - Izrada po meri',
