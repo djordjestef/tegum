950ee941557ecd903968a384bc241d6f200e823a
djordje <djordje.stefanovic89@gmail.com>
2023-12-21 09:30:09 +0100

    back browser force reload

--- main.js
+++ main.js
@@ -368,3 +368,13 @@ document.addEventListener('change', function (event) {
     element.classList[element.value ? 'add' : 'remove']('-hasvalue');
   }
 });
+//back browser force reload - safari
+window.addEventListener('pageshow', function (event) {
+  var historyTraversal =
+    event.persisted ||
+    (typeof window.performance != 'undefined' && window.performance.navigation.type === 2);
+  if (historyTraversal) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
